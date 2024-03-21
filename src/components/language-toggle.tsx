8e0ae1107f39a82834a772bd916727dc00b747ce
Bartosz Szkolnik <bartosz.szkolnik@protonmail.com>
2024-03-21 14:59:08 +0100

    Add support for browsers that can't display emojis

--- src/components/language-toggle.tsx
+++ src/components/language-toggle.tsx
@@ -6,10 +6,17 @@ import { useRouter, useSelectedLayoutSegments } from 'next/navigation';
 import { Icon } from './ui/icon';
 import { Button } from './ui/button';
 import { DictSection } from '../app/types';
+import { supportsEmojiIcons } from '@lib/utils';
+import { useEffect, useState } from 'react';
 
 export function LanguageToggle({ locale, dict }: { locale: Locale; dict: DictSection<'SETTINGS'> }) {
   const router = useRouter();
   const urlSegments = useSelectedLayoutSegments();
+  const [supportsEmoji, setSupportsEmoji] = useState(false);
+
+  useEffect(() => {
+    setSupportsEmoji(supportsEmojiIcons());
+  }, []);
 
   function handleLocaleChange(locale: Locale) {
     router.replace(`/${locale}/${urlSegments.join('/')}`, { scroll: false });
@@ -20,8 +27,16 @@ export function LanguageToggle({ locale, dict }: { locale: Locale; dict: DictSec
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
           <Icon variant="custom">
-            {(locale === 'en' || locale === 'en-US' || locale === 'en-GB') && <span className="text-2xl">🇬🇧</span>}
-            {(locale === 'pl' || locale === 'pl-PL') && <span className="text-2xl">🇵🇱</span>}
+            {supportsEmoji ? (
+              <>
+                {(locale === 'en' || locale === 'en-US' || locale === 'en-GB') && <span className="text-2xl">🇬🇧</span>}
+                {(locale === 'pl' || locale === 'pl-PL') && <span className="text-2xl">🇵🇱</span>}
+              </>
+            ) : locale === 'pl' || locale === 'pl-PL' ? (
+              <Icon variant="polish-flag" className="h-[1.2rem] w-[1.2rem]" />
+            ) : (
+              <Icon variant="great-britain-flag" className="h-[1.2rem] w-[1.2rem]" />
+            )}
             <span className="sr-only">{dict.CHOOSE_LANGUAGE}</span>
           </Icon>
         </Button>
@@ -29,13 +44,21 @@ export function LanguageToggle({ locale, dict }: { locale: Locale; dict: DictSec
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => handleLocaleChange('en-GB')}>
           <span className="flex items-center gap-2">
-            <span className="text-2xl">🇬🇧</span>
+            {supportsEmoji ? (
+              <span className="text-2xl">🇬🇧</span>
+            ) : (
+              <Icon variant="great-britain-flag" className="h-[1.2rem] w-[1.2rem]" />
+            )}
             {dict.ENGLISH}
           </span>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => handleLocaleChange('pl-PL')}>
           <span className="flex items-center gap-2">
-            <span className="text-2xl">🇵🇱</span>
+            {supportsEmoji ? (
+              <span className="text-2xl">🇵🇱</span>
+            ) : (
+              <Icon variant="polish-flag" className="h-[1.2rem] w-[1.2rem]" />
+            )}
             {dict.POLISH}
           </span>
         </DropdownMenuItem>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,28 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+// this code is from https://stackoverflow.com/questions/45576748/how-can-i-detect-rendering-support-for-emoji-in-javascript
+// don't know how it works under the hood, but it gets the job done
+// please don't mindlessly copy
+export function supportsEmojiIcons() {
+  const canvas = document.createElement('canvas');
+  canvas.height = 10;
+  canvas.width = canvas.height * 2;
+
+  const ctx = canvas.getContext('2d')!;
+  ctx.font = canvas.height + 'px Arial';
+  ctx.fillText('🇬🇧', 0, canvas.height);
+
+  const data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+  let i = 0;
+  while (i < data.length) {
+    if (data[i] !== data[i + 1] || data[i] !== data[i + 2]) return true;
+    i += 4;
+  }
+
+  return false;
 }
